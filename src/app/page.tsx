ed4770c255f7adb182fbb0bc1ef6f9bfae75ef60
Valentin <valentin@Mac-mini-de-Holylab.local>
2023-11-10 09:29:43 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="layout">hello home</div>
+    <div className="layout">created a new app</div>
   )
 }
